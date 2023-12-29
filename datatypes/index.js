b71daa86030d465d463e4d283be6e0ae3d26cac7
MOHIT <76442921+mohit9053@users.noreply.github.com>
2023-12-29 17:14:31 +0000

    datatypess

---
+++ datatypes/index.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as a newer version
+
+// alert(3+3) // we are using nodejs, not browser
+// number
+// string
+// boolean
+// null = standalone value
+// undefined = not assigned
+// symbol = unique
+
+
+// object 
---
+++ variables/index.js
@@ -0,0 +1,16 @@
+const accountId = 1445
+let email = "mohit@gmail.com"
+var accountPass = "1234"
+acccountCity = "rewari"
+
+// accountId = 2; // not allowed
+email="mohiiii"
+accountPass="789"
+acccountCity="lucknow"
+// console.log(accountId);
+
+console.log([accountId,accountPass,email,acccountCity]);
+
+/*
+prefer not to use var because of issue in block scope and function scope
+*/
