f8dc01a08163cb4043870993cf73d7041e5322f6
MOHIT <76442921+mohit9053@users.noreply.github.com>
2023-12-30 16:22:58 +0000

    comparision

---
+++ conversionandoperation/index.js
@@ -0,0 +1,25 @@
+let score = "33";
+
+console.log(typeof score);
+let num = Number(score);
+console.log(typeof num);
+console.log(num);
+
+// "33" => 33
+// "33abs" => NaN
+// typeof NaN is Number
+
+// OPERATIONS
+let value=3;
+// console.log(-value);
+
+let value2 = "1"+2;
+let value3 = 1+2+"1";
+let value4 = "1"+2+4;
+console.log(typeof value2);
+console.log(value3);
+console.log(typeof value3);
+console.log(value4);
+console.log(typeof value4);
+
+console.log(+"") 
