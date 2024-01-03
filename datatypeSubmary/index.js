e705a0180e42614458de706367d069bdf57aafd8
MOHIT <76442921+mohit9053@users.noreply.github.com>
2024-01-03 17:17:27 +0000

    datasumary

---
+++ datatypeSubmary/index.js
@@ -0,0 +1,14 @@
+// 2 catogary
+// kaise rakhe hai data base me or kaise access kr rhe hai 
+// primitive or non-primitive
+
+// primitive 7 - string,number, boolean, null, undefined, symbol - to make unique, BigInt
+// reference (non primitive) : arrays, objects, functions
+
+// dynamic type language - javascript
+
+
+const id = Symbol('123')
+const another = Symbol('123')
+console.log(id===another); // false
+
