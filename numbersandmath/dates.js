44f515d74bace1d2ea7df90febd4763935c6c061
MOHIT <76442921+mohit9053@users.noreply.github.com>
2024-01-06 16:16:28 +0000

    basics done

---
+++ numbersandmath/dates.js
@@ -0,0 +1,6 @@
+let n = new Date();
+console.log(n.toDateString());
+console.log(n.toLocaleString());
+
+let d = new Date(2023,1,23)
+console.log(d.toLocaleDateString());
---
+++ numbersandmath/index.js
@@ -0,0 +1,19 @@
+const score = 400;
+const n = new Number(100);
+console.log(n);
+console.log(score);
+
+
+console.log(n.toFixed(3));
+//precision ke priority starting se dekhte hai 
+
+const nn = 1000000
+console.log(nn.toLocaleString('en-IN'));
+
+
+// maths -javascript me default aate hai 
+
+console.log(Math.floor(13.55));
+// round, floor, ceil
+console.log(Math.floor(Math.sqrt(26)));
+const min = 10;
